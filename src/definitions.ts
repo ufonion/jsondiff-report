5f0e0e01672e4371f7ca0d159e94e57e25ef081f
Marshall Ma <marshall.ma@ericsson.com>
2019-08-13 19:59:29 +0800

    0.0.4

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,14 +1,43 @@
+/**
+ * File list
+ */
 export interface FileList {
+  /**
+   * The base directory of the file list.
+   */
   baseDir: string;
+  /**
+   * The list of files under the {@link FileList.baseDir}.
+   */
   files: string[];
 }
 
+/**
+ * Base dir diff
+ */
+export interface BaseDirDiff {
+  left: string;
+  right: string;
+}
+
+/**
+ * File list diff
+ */
 export interface FileListDiff {
-  baseDir: {
-    left: string;
-    right: string;
-  };
+  /**
+   * The base directories of 2 diff target.
+   */
+  baseDir: BaseDirDiff;
+  /**
+   * The list of files that are only under {@link BaseDirDiff.left}.
+   */
   leftOnly: string[];
+  /**
+   * The list of files that are only under {@link BaseDirDiff.right}.
+   */
   rightOnly: string[];
+  /**
+   * The list of files that are under both {@link BaseDirDiff.left} and {@link BaseDirDiff.right}.
+   */
   all: string[];
 }
--- src/fileList.ts
+++ src/fileList.ts
@@ -2,8 +2,13 @@ import { FileList, FileListDiff } from './definitions';
 // tslint:disable-next-line: no-submodule-imports
 import { readdir, stat } from 'mz/fs';
 import * as path from 'path';
-
-async function traversePath(base: string, func: (file: string) => void): Promise<void> {
+/**
+ * Traverse a path and call a function to every file found under the path
+ * @param  {string} base
+ * @param  {(file:string)=>void} func
+ * @returns Promise<void>
+ */
+export async function traversePath(base: string, func: (file: string) => void): Promise<void> {
   const files = await readdir(base);
 
   for (const file of files) {
@@ -17,7 +22,11 @@ async function traversePath(base: string, func: (file: string) => void): Promise
     }
   }
 }
-
+/**
+ * Generate a file list under a base directory
+ * @param  {string} baseDir
+ * @returns Promise
+ */
 export async function generateFileList(baseDir: string): Promise<FileList> {
   let normalizedBase = path.normalize(baseDir);
   if (normalizedBase.charAt(normalizedBase.length - 1) !== path.sep) {
@@ -36,7 +45,12 @@ export async function generateFileList(baseDir: string): Promise<FileList> {
 
   return fileList;
 }
-
+/**
+ * Generate a diff {@link FileListDiff} for 2 instance of {@link FileList}
+ * @param  {FileList} left
+ * @param  {FileList} right
+ * @returns FileListDiff
+ */
 export function generateFileListDiff(left: FileList, right: FileList): FileListDiff {
   return {
     baseDir: {
